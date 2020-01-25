ccb0900dccba6e796a7f60fc227d3ba82942770e
Aniket Kadam <anikadamg@gmail.com>
2020-01-25 12:39:44 +0530

    Implemented application injection

--- app/src/main/java/com/aniketkadam/heritageplaces/HeritagePlacesApplication.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/HeritagePlacesApplication.kt
@@ -1,7 +1,15 @@
 package com.aniketkadam.heritageplaces
 
-import android.app.Application
+import com.aniketkadam.heritageplaces.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 
-class HeritagePlacesApplication : Application() {
+class HeritagePlacesApplication : DaggerApplication() {
+
+    private val appInjector by lazy {
+        DaggerAppComponent.factory().create(this)
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> = appInjector
 
 }
