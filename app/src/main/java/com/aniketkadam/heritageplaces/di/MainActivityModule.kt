e7a24b5fe58d51b4f3eebf0b44dba709e4e25348
Aniket Kadam <anikadamg@gmail.com>
2020-01-25 12:26:20 +0530

    Created a dagger module for MainVm to be shared between fragments

---
+++ app/src/main/java/com/aniketkadam/heritageplaces/di/MainActivityModule.kt
@@ -0,0 +1,27 @@
+package com.aniketkadam.heritageplaces.di
+
+import androidx.lifecycle.ViewModelProviders
+import com.aniketkadam.heritageplaces.MainActivity
+import com.aniketkadam.heritageplaces.MainVm
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+
+const val MAINACTIVITY_VM = "main_activity_vm"
+const val MAINACTIVITY_FRAGMENT_VM = "main_activity_fragment_vm"
+
+@Module
+object MainActivityModule {
+
+    @JvmStatic
+    @Provides
+    @Named(MAINACTIVITY_VM)
+    fun provideVm(mainActivity: MainActivity, factory: MainVmFactory): MainVm =
+        ViewModelProviders.of(mainActivity, factory).get(MainVm::class.java)
+
+    @JvmStatic
+    @Provides
+    @Named(MAINACTIVITY_FRAGMENT_VM)
+    fun provideFragmentVm(mainActivity: MainActivity): MainVm =
+        ViewModelProviders.of(mainActivity).get(MainVm::class.java)
+}
