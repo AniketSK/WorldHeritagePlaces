1c3cc4b73bb393a3db228d75d7123aafe2b42570
Aniket Kadam <anikadamg@gmail.com>
2020-01-25 10:49:07 +0530

    Data class for the heritage location info

---
+++ app/src/main/java/com/aniketkadam/heritageplaces/data/HeritagePlace.kt
@@ -0,0 +1,8 @@
+package com.aniketkadam.heritageplaces.data
+
+data class HeritagePlace(
+    val name: String,
+    val image: String,
+    val shortInfo: String,
+    val location: Location
+)
---
+++ app/src/main/java/com/aniketkadam/heritageplaces/data/Location.kt
@@ -0,0 +1,6 @@
+package com.aniketkadam.heritageplaces.data
+
+data class Location(
+    val lat: Double,
+    val lng: Double
+)
