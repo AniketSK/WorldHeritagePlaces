9d6d8678567d2fe04ebc6dcc42472820b27d89d4
Aniket Kadam <anikadamg@gmail.com>
2020-01-25 20:43:49 +0530

    Connected the PagedAdapter to recyclerview, items should be seen now Also named the recyclervew in the heritage list so it can be referenced

--- app/src/main/java/com/aniketkadam/heritageplaces/heritageplacelist/HeritageList.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/heritageplacelist/HeritageList.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import com.aniketkadam.heritageplaces.MainVm
+import com.aniketkadam.heritageplaces.PagedHeritagePlaceAdapter
 import com.aniketkadam.heritageplaces.R
+import com.aniketkadam.heritageplaces.ScreenLce
+import com.aniketkadam.heritageplaces.data.HeritagePlace
 import com.aniketkadam.heritageplaces.di.MAINACTIVITY_FRAGMENT_VM
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.layout_heritage_list.*
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -17,11 +23,29 @@ class HeritageList : DaggerFragment() {
     @field:Named(MAINACTIVITY_FRAGMENT_VM)
     lateinit var mainVm: MainVm
 
+    private val adapter = PagedHeritagePlaceAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.layout_heritage_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        heritageListRecyclerview.adapter = adapter
+        mainVm.viewState.observe(viewLifecycleOwner, Observer { renderState(it) })
+    }
+
+    private fun renderState(viewState: ScreenLce?) {
+        if (viewState is ScreenLce.Content) {
+            viewState.heritagePagedList.observe(viewLifecycleOwner, Observer { renderList(it) })
+        }
+    }
+
+    private fun renderList(pagedList: PagedList<HeritagePlace>?) {
+        adapter.submitList(pagedList)
     }
 }
