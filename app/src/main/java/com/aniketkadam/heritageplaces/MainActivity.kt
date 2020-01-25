513b1e28568bac15586e3213c5efbd4875c91b98
Aniket Kadam <anikadamg@gmail.com>
2020-01-25 13:01:51 +0530

    Directing the screen to loading or error based on the VM view state
    The safe navgiate function is required to prevent the nav component
    from crashing when it tries to go from the same fragment to itself,
    such as when the screen is rotated.

--- app/src/main/java/com/aniketkadam/heritageplaces/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.aniketkadam.heritageplaces
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import com.aniketkadam.heritageplaces.di.MAINACTIVITY_VM
+import com.aniketkadam.heritageplaces.loadingscreen.LoadingFragmentDirections
 import dagger.android.support.DaggerAppCompatActivity
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -15,5 +21,23 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainVm.viewState.observe(this, Observer { setActiveFragment(it) })
     }
+
+    private fun setActiveFragment(viewState: ScreenLce?) = with(findNavController(R.id.nav_host)) {
+        when (viewState) {
+            ScreenLce.Loading -> {
+                Timber.d("Is loading")
+            } // Loading is the home fragment and currently we never return to it so nothing happens.
+
+            ScreenLce.Error -> safeNavigate(LoadingFragmentDirections.actionLoadingFragmentToErrorFragment())
+
+        }
+    }
+
 }
+
+private fun NavController.safeNavigate(d: NavDirections) =
+    currentDestination?.getAction(d.actionId)?.let { navigate(d) }
+        ?: Timber.e("Invalid route for direction ${d} with id ${d.actionId}")
