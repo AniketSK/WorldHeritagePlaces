d6c9d5c4153665d50d0a63e49ceedbc96218a75d
Aniket Kadam <anikadamg@gmail.com>
2020-01-26 14:47:29 +0530

    Using the passed in heritage place arg to go to its map location

--- app/src/main/java/com/aniketkadam/heritageplaces/maps/MapsFragment.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/maps/MapsFragment.kt
@@ -1,11 +1,17 @@
 package com.aniketkadam.heritageplaces.maps
 
 import android.os.Bundle
+import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : SupportMapFragment(), OnMapReadyCallback {
+
+    private val args by navArgs<MapsFragmentArgs>()
 
     override fun onActivityCreated(p0: Bundle?) {
         super.onActivityCreated(p0)
@@ -13,7 +19,13 @@ class MapsFragment : SupportMapFragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        TODO("handle the marker showing up for the place")
+        val place = args.heritagePlace
+
+        val mapsLocation = LatLng(place.location.lat, place.location.lng)
+
+        googleMap.addMarker(MarkerOptions().position(mapsLocation).title(place.name))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(mapsLocation))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(19f)) // Zoom in a bit, range from out(2), to in(21)
     }
 
 }
