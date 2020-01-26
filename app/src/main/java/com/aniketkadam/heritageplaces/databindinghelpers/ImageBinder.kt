a3144ff72fa66a45e872a2c8534bc4e085cfaff6
Aniket Kadam <anikadamg@gmail.com>
2020-01-26 10:43:00 +0530

    Added loading and error images to the heritage view.

--- app/src/main/java/com/aniketkadam/heritageplaces/databindinghelpers/ImageBinder.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/databindinghelpers/ImageBinder.kt
@@ -1,14 +1,27 @@
 package com.aniketkadam.heritageplaces.databindinghelpers
 
+import android.content.Context
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.aniketkadam.heritageplaces.R
 import com.bumptech.glide.Glide
 
 object ImageBinder {
 
+    private fun getSpinner(context: Context) = CircularProgressDrawable(context).apply {
+        strokeWidth = 5f
+        centerRadius = 30f
+        start()
+    }
+
     @JvmStatic
     @BindingAdapter("glideSrc")
     fun setImageUrl(view: ImageView, glideSrc: String) {
-        Glide.with(view.context).load(glideSrc).into(view)
+        Glide.with(view.context)
+            .load(glideSrc)
+            .error(R.drawable.broken_image)
+            .placeholder(getSpinner(view.context))
+            .into(view)
     }
 }
