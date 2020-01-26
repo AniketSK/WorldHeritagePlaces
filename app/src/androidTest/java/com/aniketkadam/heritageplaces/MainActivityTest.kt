c707f4fc12a0811ac667ffbb28f9818efa4bb34a
Aniket Kadam <anikadamg@gmail.com>
2020-01-26 15:09:46 +0530

    Added a test to check that maps opens if the place is clicked

--- app/src/androidTest/java/com/aniketkadam/heritageplaces/MainActivityTest.kt
+++ app/src/androidTest/java/com/aniketkadam/heritageplaces/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package com.aniketkadam.heritageplaces
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
@@ -32,4 +34,13 @@ class MainActivityTest {
         )
     }
 
+    @Test
+    fun clicking_on_an_item_opens_its_maps_location() {
+        sleep(2000) // Normally I'd use an Espresso Idling Resource, demonstrated here https://github.com/AniketSK/ArchitectureExperiments/blob/bf6ddf2becac1a49027c35b135c934fe0e30922c/app/src/androidTest/java/com/aniketkadam/temperatureapp/MainActivityTest.kt#L44
+        onView(withId(R.id.heritageListRecyclerview)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<HeritageViewHolder>(1, click())
+        )
+
+        onView(withContentDescription("Google Map")).check(matches(isDisplayed()))
+    }
 }
