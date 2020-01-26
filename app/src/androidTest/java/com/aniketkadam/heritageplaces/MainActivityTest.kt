f9a46e531df6e5469537d4edbfd959cbe97ac1f2
Aniket Kadam <anikadamg@gmail.com>
2020-01-26 16:34:03 +0530

    Layout improvements Since every short description contains the name and a newline at the top there's no need to have another textview with the name, only the short description will do. Adjusted the tests to pass still.

--- app/src/androidTest/java/com/aniketkadam/heritageplaces/MainActivityTest.kt
+++ app/src/androidTest/java/com/aniketkadam/heritageplaces/MainActivityTest.kt
@@ -21,7 +21,7 @@ class MainActivityTest {
         sleep(2000) // Normally I'd use an Espresso Idling Resource, demonstrated here https://github.com/AniketSK/ArchitectureExperiments/blob/bf6ddf2becac1a49027c35b135c934fe0e30922c/app/src/androidTest/java/com/aniketkadam/temperatureapp/MainActivityTest.kt#L44
         onView(withId(R.id.heritageListRecyclerview)).perform(
             RecyclerViewActions.actionOnItem<HeritageViewHolder>(
-                hasDescendant(withText("Aachen Cathedral ")), scrollTo()
+                hasDescendant(withSubstring("Aachen Cathedral ")), scrollTo()
             )
         )
     }
