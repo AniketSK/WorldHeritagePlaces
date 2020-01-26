741d400537f25f3983ee24ede2f570d512e6cf7d
Aniket Kadam <anikadamg@gmail.com>
2020-01-26 14:47:08 +0530

    Fixed location not being added with the Heritage loader

--- app/src/main/java/com/aniketkadam/heritageplaces/HeritageListLoader.kt
+++ app/src/main/java/com/aniketkadam/heritageplaces/HeritageListLoader.kt
@@ -1,17 +1,18 @@
 package com.aniketkadam.heritageplaces
 
 import android.content.Context
-import com.aniketkadam.heritageplaces.data.HeritagePlace
+import com.aniketkadam.heritageplaces.data.HeritageDatasourceResponse
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import javax.inject.Inject
 
 class HeritageListLoader @Inject constructor(context: Context, gson: Gson) : IHeritageListLoader {
-    private val heritageListType = object : TypeToken<List<HeritagePlace>?>() {}.type
+    private val heritageListType = object : TypeToken<List<HeritageDatasourceResponse>?>() {}.type
 
     override val data by lazy {
         context.assets.open("real.planet.world-heritage.json").bufferedReader().readText()
-            .let { gson.fromJson<List<HeritagePlace>>(it, heritageListType) }
+            .let { gson.fromJson<List<HeritageDatasourceResponse>>(it, heritageListType) }
+            .map { it.getHeritagePlace() }
     }
 
 }
